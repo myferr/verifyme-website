a014ad439ce07d84d3fa3b202b281d16be6154c9
MyferIsADev <contactme.myfer@protonmail.com>
2024-07-22 18:33:36 -0500

    Fix.

--- selfhost/install.sh
+++ selfhost/install.sh
@@ -5,7 +5,7 @@ git.exe clone https://github.com/myferr/verifyme-website/
 clear
 echo "Download complete..."
 
-cd ~/verifyme-website/src
+cd verifyme-website/src
 npm install
 clear
 npm run dev -- --open
