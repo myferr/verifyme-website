27aa70be7a2e8acd9cfe2eadb26277c01a6b42c5
MyferIsADev <contactme.myfer@protonmail.com>
2024-07-22 18:08:34 -0500

    Update uninstall.ps1

--- selfhost/uninstall.ps1
+++ selfhost/uninstall.ps1
@@ -1,7 +1,7 @@
 clear
 echo "Press ENTER to confirm uninstallation."
 echo "---------"
-Remove-Item /Users/$Env:UserName/verifyme-website/
+Remove-Item -Force -Recurse -Confirm -Path="/Users/$Env:UserName/verifyme-website/"
 echo "Uninstalling.."
 clear
 echo "Successfully uninstalled!"
