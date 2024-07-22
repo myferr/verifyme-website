9dc3f9cc0d494381265ce779fdaf124c4a26e673
MyferIsADev <contactme.myfer@protonmail.com>
2024-07-22 18:28:50 -0500

    Add Bash selfhost scripts

---
+++ selfhost/install.sh
@@ -0,0 +1,11 @@
+clear
+print "Downloading.."
+cd ~
+git.exe clone https://github.com/myferr/verifyme-website/
+clear
+echo "Download complete..."
+
+cd ~/verifyme-website/src
+npm install
+clear
+npm run dev -- --open
---
+++ selfhost/uninstall.sh
@@ -0,0 +1,6 @@
+clear
+echo "Uninstalling.."
+cd ~
+rm -rf /verifyme-website
+clear
+echo "Successfully uninstalled."
