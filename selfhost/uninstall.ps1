af1d37f83553cd0b94d31b455d262fb789d6928b
MyferIsADev <contactme.myfer@protonmail.com>
2024-07-22 18:00:20 -0500

    Update uninstall.ps1

--- selfhost/uninstall.ps1
+++ selfhost/uninstall.ps1
@@ -1,2 +1,13 @@
 clear
+echo "Press ENTER to confirm uninstallation."
+echo "---------"
 Remove-Item /Users/$Env:UserName/verifyme-website/
+echo "Uninstalling.."
+clear
+$Folder = 'C:/$Env:UserName/verifyme-website'
+
+if (Test-Path -Path $Folder) {
+    "Uninstallation failed, please run the command again."
+} else {
+    "Successfully uninstalled!"
+}
